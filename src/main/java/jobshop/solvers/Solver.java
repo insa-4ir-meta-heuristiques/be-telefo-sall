c0347750b8ceb965324baf5763adb0078c70129c
Damso12032 <99388067+Damso12032@users.noreply.github.com>
2023-04-29 18:55:43 +0200

    end of tp

--- src/main/java/jobshop/solvers/Solver.java
+++ src/main/java/jobshop/solvers/Solver.java
@@ -2,6 +2,8 @@ package jobshop.solvers;
 
 import jobshop.Instance;
 import jobshop.encodings.Schedule;
+import jobshop.solvers.neighborhood.Neighborhood;
+import jobshop.solvers.neighborhood.Nowicki;
 
 import java.util.Optional;
 
@@ -19,10 +21,24 @@ public interface Solver {
 
     /** Static factory method to create a new solver based on its name. */
     static Solver getSolver(String name) {
+        GreedySolver solver=new GreedySolver(GreedySolver.Priority.EST_SPT);
+        Nowicki nowicki=new Nowicki();
+        //paramétres des tabous
+        int maxIter=50;
+        int timeTabouInit=5;
+
         switch (name) {
             case "basic": return new BasicSolver();
             case "spt": return new GreedySolver(GreedySolver.Priority.SPT);
-            // TODO: add new solvers
+            case "lrpt":return new GreedySolver(GreedySolver.Priority.LRPT);
+            case "est_lrpt":return new GreedySolver(GreedySolver.Priority.EST_LRPT);
+            case "est_spt":return new GreedySolver(GreedySolver.Priority.EST_SPT);
+            case "est_lpt_random":return new GreedySolver(GreedySolver.Priority.EST_LPT_RANDOM);
+            case "est_lrpt_random": return new GreedySolver(GreedySolver.Priority.EST_LRPT_RANDOM);
+            case "descent_est_spt":return new DescentSolver(nowicki,new GreedySolver(GreedySolver.Priority.EST_SPT));
+            case "descent_est_lrpt":return new DescentSolver(nowicki,new GreedySolver(GreedySolver.Priority.EST_LRPT));
+            case "tabou_est_spt":return new TabooSolver(nowicki,new GreedySolver(GreedySolver.Priority.EST_SPT),maxIter,2,timeTabouInit);
+            case "tabou_est_lrpt":return new TabooSolver(nowicki,new GreedySolver(GreedySolver.Priority.EST_LRPT),maxIter,2,timeTabouInit);
             //case "dct": return new DescentSolver();
             default: throw new RuntimeException("Unknown solver: "+ name);
         }
